4186e08802744a1aeb4add43947ea245bb3a1834
Rocci <mdcoraccio@gmail.com>
2023-04-08 15:37:12 -0400

    more gecko code updates for skip opening scene

--- Source/GeckoCodes/AvoidOpeningExceptions.asm
+++ Source/GeckoCodes/AvoidOpeningExceptions.asm
@@ -101,3 +101,165 @@ FINALLY:
   addis r4, r31, 3
   addi r3, r3, 2680
   blr
+
+#To be inserted at 8027b57c
+IS_OPENING_PLAYING:
+  lis r30, 0x8060
+  lbz r30, 0 (r30)
+  cmpwi r30, 0x4
+  lis r30, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r30, r30, 0xb5a0 # i don't know why i'm skipping to here
+  li r3, 0  # i don't know why i have to do this
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r30, r30, 0xb580
+
+FINALLY:
+  mtlr r30
+  li r30, 0
+  blr
+
+#To be inserted at 8027b5a0
+IS_OPENING_PLAYING:
+  lis r4, 0x8060
+  lbz r4, 0 (r4)
+  cmpwi r4, 0x4
+  lis r4, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r4, r4, 0xb5c8
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r4, r4, 0xb5a4
+
+FINALLY:
+  mtlr r4
+  addis r4, r29, 3
+  blr
+
+#To be inserted at 8027b5dc
+IS_OPENING_PLAYING:
+  lis r3, 0x8060
+  lbz r3, 0 (r3)
+  cmpwi r3, 0x4
+  lis r3, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r3, r3, 0xb5e4
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r3, r3, 0xb5e0
+
+FINALLY:
+  mtlr r3
+  lfs f1, -0x53e8 (rtoc)
+  blr
+
+#To be inserted at 8027b604
+IS_OPENING_PLAYING:
+  lis r3, 0x8060
+  lbz r3, 0 (r3)
+  cmpwi r3, 0x4
+  lis r3, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r3, r3, 0xb608
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r3, r3, 0xb60c
+
+FINALLY:
+  mtlr r3
+  lfs f1, -0x53e8 (rtoc)
+  blr
+
+#To be inserted at 8027b618
+IS_OPENING_PLAYING:
+  lis r3, 0x8060
+  lbz r3, 0 (r3)
+  cmpwi r3, 0x4
+  lis r3, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r3, r3, 0xb624
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r3, r3, 0xb61c
+
+FINALLY:
+  mtlr r3
+  addis r3, r31, 3
+  blr
+
+#To be inserted at 8027b628
+IS_OPENING_PLAYING:
+  lis r3, 0x8060
+  lbz r3, 0 (r3)
+  cmpwi r3, 0x4
+  lis r3, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r3, r3, 0xb630
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r3, r3, 0xb62c
+
+FINALLY:
+  mtlr r3
+  addis r3, r31, 3
+  blr
+
+#To be inserted at 8027b644
+IS_OPENING_PLAYING:
+  lis r3, 0x8060
+  lbz r3, 0 (r3)
+  cmpwi r3, 0x4
+  lis r3, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r3, r3, 0xb64c
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r3, r3, 0xb648
+
+FINALLY:
+  mtlr r3
+  addis r3, r31, 3
+  lfs f0, -0x53e8 (rtoc)
+  blr
+
+#To be inserted at 8027b64c
+IS_OPENING_PLAYING:
+  lis r4, 0x8060
+  lbz r4, 0 (r4)
+  cmpwi r4, 0x4
+  lis r4, 0x8027
+  bne IS_NOT_OPENING
+
+IS_OPENING:
+  ori r4, r4, 0xb688
+  b 0x8
+
+IS_NOT_OPENING:
+  ori r4, r4, 0xb648
+
+FINALLY:
+  mtlr r4
+  li r4, 0
+  blr
