a93fb2df14ae01561f5f7a837dfe19289f1c462b
Rocci <mdcoraccio@gmail.com>
2023-04-11 13:10:24 -0400

    new skip opening asm

--- Source/GeckoCodes/SkipOpening.asm
+++ Source/GeckoCodes/SkipOpening.asm
@@ -1,21 +1,3 @@
-#To be inserted at 80286c5c
-IS_OPENING_PLAYING:
-  mtlr r3
-  lis r3, 0x8060
-  lbz r3, 0 (r3)
-  cmpwi r3, 0x4
-  mflr r3
-  lis r0, 0x8028
-  bne IS_NOT_OPENING
-
-IS_OPENING:
-  ori r0, r0, 0x6c64
-  b 0x8
-
-IS_NOT_OPENING:
-  ori r0, r0, 0x6c60
-
-FINALLY:
-  mtlr r0
-  blr
-
+#To be inserted at 80284110
+  li r3, 0x1
+  stb r3, 0xb1 (r31)
