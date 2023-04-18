c66f65511088465a2d04e49333e5874f0b4952ae
Rocci <mdcoraccio@gmail.com>
2023-04-18 14:04:30 -0400

    further commenting

--- Source/GeckoCodes/All Fast Stadiums.asm
+++ Source/GeckoCodes/All Fast Stadiums.asm
@@ -7,7 +7,7 @@ SET_SPEED:
   bne- SET_SLIPPERY     # If not equal, go to SET_SLIPPERY
   lis r12, 0x3F26       # Using r12 - is this ever reset at the end?
   ori r12, r12, 0x6666  # Load 3F266666 into r12
-  stw r12, 0xc (r31)    # Store 3F266666 into 909B656C - why? what does this do?
+  stw r12, 0xc (r31)    # Store 3F266666 into 909B656C - this value equates to .65 in float
 
 SET_SLIPPERY:
   subi r11, r11, 0x10   # Set r11 to 909B6550
@@ -22,7 +22,7 @@ SET_FRICTION:
   bne- SET_BOUNCE       # If not equal, branch to SET_BOUNCE
   lis r12, 0x3E4C       
   ori r12, r12, 0xCCCD  # Load 3E4CCCCD into r12
-  stw r12, 0xc (r31)    # Store 3E4CCCCD into 909B654C
+  stw r12, 0xc (r31)    # Store 3E4CCCCD into 909B654C - this value equates to .2 in float
 
 SET_BOUNCE:
   subi r11, r11, 0x10   # Set r11 to 909B6530
@@ -30,7 +30,7 @@ SET_BOUNCE:
   bne- END              # Go to end of function - this is going to leave r11 at 909B6530 at exit time
   lis r12, 0x3F19       
   ori r12, r12, 0x999A  # Load 3f19999a into r12
-  stw r12, 0xc (r31)    # Store it into 909B6530
+  stw r12, 0xc (r31)    # Store it into 909B6530 - this value equates to .6 in float
 
 END:                    # This function runs a whole lot more than just loading in the stadium terrain
                         # Some concerns: the value of r11 and possibly r12 is overridden no matter what.
@@ -40,3 +40,4 @@ END:                    # This function runs a whole lot more than just loading 
                         # I tried breaking when 909B6560 is loaded in r31, and manually setting these values on Vice,
                         # but it was still slow. Function labels are a guess, I don't know how these values convert
                         # to .65 / 0 / .2 / .6 from the ini file
+                        # Hex to float converter here: https://gregstoll.com/~gregstoll/floattohex/
