b96e0fc45fd2162446bf5dca34d771eae3e7ef8a
Rocci <mdcoraccio@gmail.com>
2023-04-18 15:22:16 -0400

    fixes to fast stadium

--- Source/GeckoCodes/All Fast Stadiums.asm
+++ Source/GeckoCodes/All Fast Stadiums.asm
@@ -1,43 +1,38 @@
-#To be inserted at 800081EC
+#To be inserted at 800081f0
 SET_SPEED:
-  stfs f0, 0xc (r31)    # initial instruction at 800081EC
-  lis r11, 0x909B       # Using r11 - is this ever reset at the end?
-  ori r11, r11, 0x6560  # Load 909B6560 into r11
-  cmpw r31, r11         # Compare with r31
+  lis r0, 0x909B        # Using r0 - it gets set after this function anyway
+  ori r0, r0, 0x6560    # Load 909B6560 into r0
+  cmpw r0, r31          # Compare with r31
   bne- SET_SLIPPERY     # If not equal, go to SET_SLIPPERY
-  lis r12, 0x3F26       # Using r12 - is this ever reset at the end?
-  ori r12, r12, 0x6666  # Load 3F266666 into r12
-  stw r12, 0xc (r31)    # Store 3F266666 into 909B656C - this value equates to .65 in float
+  lis r0, 0x3F26       
+  ori r0, r0, 0x6666    # Load 3F266666 into r0 - this value equates to .65 in float
+  stw r0, 0xc (r31)     # and store it into 909B656C
 
 SET_SLIPPERY:
-  subi r11, r11, 0x10   # Set r11 to 909B6550
-  cmpw r31, r11         # Compare with r31
+  lis r0, 0x909B        
+  ori r0, r0, 0x6550    # Set r11 to 909B6550
+  cmpw r0, r31          # Compare with r31
   bne- SET_FRICTION     # If not equal, branch to SET_FRICTION
-  lis r12, 0x0          # Wipe r12
-  stw r12, 0xc (r31)    # Store 0 into 909B655C
+  lis r0, 0x0           # Wipe r0
+  stw r0, 0xc (r31)     # Store 0 into 909B655C
 
 SET_FRICTION:
-  subi r11, r11, 0x10   # Set r11 to 909B6540
-  cmpw r31, r11         # Compare with r31
+  lis r0, 0x909B        
+  ori r0, r0, 0x6540    # Set r11 to 909B6540
+  cmpw r0, r31          # Compare with r31
   bne- SET_BOUNCE       # If not equal, branch to SET_BOUNCE
-  lis r12, 0x3E4C       
-  ori r12, r12, 0xCCCD  # Load 3E4CCCCD into r12
-  stw r12, 0xc (r31)    # Store 3E4CCCCD into 909B654C - this value equates to .2 in float
+  lis r0, 0x3E4C       
+  ori r0, r0, 0xCCCD    # Load 3E4CCCCD into r0 - this value equates to .2 in float
+  stw r0, 0xc (r31)     # and store it into 909B654C
 
 SET_BOUNCE:
-  subi r11, r11, 0x10   # Set r11 to 909B6530
-  cmpw r31, r11         # Compare with r31
-  bne- END              # Go to end of function - this is going to leave r11 at 909B6530 at exit time
-  lis r12, 0x3F19       
-  ori r12, r12, 0x999A  # Load 3f19999a into r12
-  stw r12, 0xc (r31)    # Store it into 909B6530 - this value equates to .6 in float
+  lis r0, 0x909B        
+  ori r0, r0, 0x6530    # Set r11 to 909B6530
+  cmpw r0, r31          # Compare with r31
+  bne- END              # Go to end of function
+  lis r0, 0x3F19       
+  ori r0, r0, 0x999A    # Load 3f19999a into r0 - this value equates to .6 in float
+  stw r0, 0xc (r31)     # Store it into 909B6530
 
-END:                    # This function runs a whole lot more than just loading in the stadium terrain
-                        # Some concerns: the value of r11 and possibly r12 is overridden no matter what.
-                        # I'm not sure what those are used for, but it could be causing some issues.
-                        # r0 and r31 are much safer to use, as they're overridden right after the insertion address
-                        # Though I guess this means we lose the ability to use r31 in the way we're using it here.
-                        # I tried breaking when 909B6560 is loaded in r31, and manually setting these values on Vice,
-                        # but it was still slow. Function labels are a guess, I don't know how these values convert
-                        # to .65 / 0 / .2 / .6 from the ini file
-                        # Hex to float converter here: https://gregstoll.com/~gregstoll/floattohex/
+FINALLY:                # This function runs a whole lot more than just loading in the stadium terrain
+  lwz r31, 0xc (sp)     # Hex to float converter here: https://gregstoll.com/~gregstoll/floattohex/
