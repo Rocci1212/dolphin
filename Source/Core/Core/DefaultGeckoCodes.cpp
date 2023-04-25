bd34f5b9df18093e5c4e8cc44a6b7dc38bd1bf96
Rocci <mdcoraccio@gmail.com>
2023-04-25 10:35:52 -0400

    update codes address

--- Source/Core/Core/DefaultGeckoCodes.cpp
+++ Source/Core/Core/DefaultGeckoCodes.cpp
@@ -10,7 +10,7 @@ void DefaultGeckoCodes::RunCodeInject(bool bCompatibilityMode, bool bNetplayMode
   // Memory::Write_U32(0x2c1f0023, 0x80095914);
   // Memory::Write_U32(0x3be00003, 0x8009591c);
 
-  aWriteAddr = 0x80600050;  // starting asm write addr (just a lot of blank space)
+  aWriteAddr = 0x80603000;  // starting asm write addr (just a lot of blank space)
 
   // handle asm writes for required C2 codes
   for (DefaultGeckoCode geckocode : sRequiredCodes)
