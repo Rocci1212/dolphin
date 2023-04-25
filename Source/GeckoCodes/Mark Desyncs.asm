f95642d257e2e2f7534e8bfa25b788ec0d9c0fc1
Rocci <mdcoraccio@gmail.com>
2023-04-25 09:05:03 -0400

    update mark desyncs code, version.cpp

--- Source/GeckoCodes/Mark Desyncs.asm
+++ Source/GeckoCodes/Mark Desyncs.asm
@@ -1,15 +1,15 @@
 #To be inserted at 803332D8
 # Mark Desyncs
 # Code should write a value to memory when the game desyncs
-  cmplw	r3, r0 # our original instruction is a compare
-  lis r14, 0x8060 # load empty space in memory
-  beq WEGOOD # skip this step if our CRCs are in sync
-  li r15, 1 # runs only if CRCs not in sync
+  cmplw	r3, r0        # our original instruction is a compare
+  lis r14, 0x8060     # load empty space in memory
+  beq WEGOOD          # skip this step if our CRCs are in sync
+  li r25, 1           # runs only if CRCs not in sync - store 1 in memory
   b END
 WEGOOD:
-  li r15, 0
+  li r25, 0           # there's no issue - store this in memory
 END:
-  stw r15, 0x10(r14) # write r15 (0 if sync'ed, 1 if desynced) to indicate desync in memory
-  lis r14, 0 # wipe r14
-  lis r15, 0 # wipe r15
-  cmpw r14, r15 # this should skip the game's desync check
+  stw r25, 0x10(r3)   # write r25 (0 if sync'ed, 1 if desynced) to indicate desync in memory
+  lis r25, 1          # wipe r14
+  lwz r3, 0x11c (r26) # wipe r15
+  cmpw r3, r3         # this should skip the game's desync check
