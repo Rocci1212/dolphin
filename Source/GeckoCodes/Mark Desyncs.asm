b8d18d01921582f3f3db8510333751b3c266c261
Rocci <mdcoraccio@gmail.com>
2023-04-25 09:27:48 -0400

    default gecko codes

--- Source/GeckoCodes/Mark Desyncs.asm
+++ Source/GeckoCodes/Mark Desyncs.asm
@@ -2,14 +2,16 @@
 # Mark Desyncs
 # Code should write a value to memory when the game desyncs
   cmplw	r3, r0        # our original instruction is a compare
-  lis r14, 0x8060     # load empty space in memory
+  lis r3, 0x8060      # load empty space in memory
   beq WEGOOD          # skip this step if our CRCs are in sync
   li r25, 1           # runs only if CRCs not in sync - store 1 in memory
   b END
+
 WEGOOD:
   li r25, 0           # there's no issue - store this in memory
+
 END:
   stw r25, 0x10(r3)   # write r25 (0 if sync'ed, 1 if desynced) to indicate desync in memory
-  lis r25, 1          # wipe r14
-  lwz r3, 0x11c (r26) # wipe r15
+  li r25, 1           # instruction at 803332cc
+  lwz r3, 0x11c (r26) # instruction at 803332d0
   cmpw r3, r3         # this should skip the game's desync check
