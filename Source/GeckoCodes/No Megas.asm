3fe61d46925bd25fbe7f9b91a87bcac2eea4d0b1
Rocci <mdcoraccio@gmail.com>
2023-06-23 00:41:37 -0400

    update notes on No Megas code

--- Source/GeckoCodes/No Megas.asm
+++ Source/GeckoCodes/No Megas.asm
@@ -1,4 +1,9 @@
 #To be inserted at 800FCA80
+# Updates to be made to this
+# I need to know if I'm online or offline
+# If offline, this code works fine
+# If online, that is to say, word? at 80c5f340 = 1
+# Then I need to set 80c5f29c to 3, not 80c5f304 as below
 SHOT_RELEASE:
   mr r0, r3
   lis r3, 0x8060
