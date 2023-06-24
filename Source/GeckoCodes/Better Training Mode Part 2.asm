443886f09feab1e10220eae4b19828b3765f395a
Rocci <mdcoraccio@gmail.com>
2023-06-24 12:08:14 -0400

    updates to better training mode part 2 to compile in codewrite

--- Source/GeckoCodes/Better Training Mode Part 2.asm
+++ Source/GeckoCodes/Better Training Mode Part 2.asm
@@ -1,80 +1,80 @@
 loc_0x0:
   lis r9, 0x8000
-  lbz r3, 438(r9)
+  lbz r3, 0x1b6 (r9)
   cmpwi r3, 0x0
-  beq+ loc_0xEC
+  beq+ loc_0x204
   lis r12, 0x8058
-  lhz r11, 24290(r12)
-  lbz r5, 5456(r9)
-  lbz r10, 5457(r9)
+  lhz r11, 0x5EE2 (r12)
+  lbz r5, 0x1550 (r9)
+  lbz r10, 0x1551 (r9)
   cmpwi r5, 0xFF
-  bne- loc_0x2C
-  li r5, 0xFFFF
+  bne- loc_0x144
+  li r5, 0xffffFFFF
 
-loc_0x2C:
+loc_0x144:
   cmpwi r10, 0x0
-  bne- loc_0x38
+  bne- loc_0x150
   li r10, 0x1
 
-loc_0x38:
-  lbz r12, 5458(r9)
+loc_0x150:
+  lbz r12, 0x1552 (r9)
   cmpwi r12, 0x0
-  bne- loc_0xA4
+  bne- loc_0x1BC
   cmpwi r11, 0x101
-  bne+ loc_0x5C
+  bne+ loc_0x174
   subi r5, r5, 0x1
-  cmpwi r5, 0xFFFE
-  bne- loc_0x5C
+  cmpwi r5, 0xffffFFFE
+  bne- loc_0x174
   li r5, 0x14
 
-loc_0x5C:
+loc_0x174:
   cmpwi r11, 0x102
-  bne+ loc_0x74
+  bne+ loc_0x18C
   addi r5, r5, 0x1
   cmpwi r5, 0x15
-  bne- loc_0x74
-  li r5, 0xFFFF
+  bne- loc_0x18C
+  li r5, 0xffffFFFF
 
-loc_0x74:
+loc_0x18C:
   cmpwi r11, 0x104
-  bne+ loc_0x8C
+  bne+ loc_0x1A4
   subi r10, r10, 0x1
   cmpwi r10, 0x0
-  bne- loc_0x8C
+  bne- loc_0x1A4
   li r10, 0x19
 
-loc_0x8C:
+loc_0x1A4:
   cmpwi r11, 0x108
-  bne+ loc_0xA4
+  bne+ loc_0x1BC
   addi r10, r10, 0x1
   cmpwi r10, 0x1A
-  bne- loc_0xA4
+  bne- loc_0x1BC
   li r10, 0x1
 
-loc_0xA4:
+loc_0x1BC:
   li r12, 0x0
   andi. r0, r11, 0xF
-  beq+ loc_0xB4
+  beq+ loc_0x1CC
   li r12, 0x1
 
-loc_0xB4:
-  stb r5, 5456(r9)
-  stb r10, 5457(r9)
-  stb r12, 5458(r9)
-  li r11, 0xFFFF
+loc_0x1CC:
+  stb r5, 0x1550 (r9)
+  stb r10, 0x1551 (r9)
+  stb r12, 0x1552 (r9)
+  li r11, 0xffffFFFF
   lis r12, 0x80CF
   cmpwi r3, 0x1
-  beq- loc_0xE0
-  stw r5, 22908(r12)
-  stw r10, 22912(r12)
-  stw r11, 25828(r12)
-  b loc_0xEC
+  beq- loc_0x1F8
+  stw r5, 0x597C (r12)
+  stw r10, 0x5980 (r12)
+  stw r11, 0x64E4 (r12)
+  b loc_0x204
 
-loc_0xE0:
-  stw r5, 25828(r12)
-  stw r10, 25832(r12)
-  stw r11, 22908(r12)
+loc_0x1F8:
+  stw r5, 0x64E4 (r12)
+  stw r10, 0x64E8 (r12)
+  stw r11, 0x597C (r12)
 
-loc_0xEC:
+loc_0x204:
   blr 
 
