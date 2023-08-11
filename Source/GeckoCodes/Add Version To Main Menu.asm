dd9a20a7684638a3857e0928d90165209427fc7a
Rocci <mdcoraccio@gmail.com>
2023-08-11 03:33:21 -0400

    text hacking gone wrong

--- Source/GeckoCodes/Add Version To Main Menu.asm
+++ Source/GeckoCodes/Add Version To Main Menu.asm
@@ -4,8 +4,9 @@
   stmw r14, 0x8 (sp)     # push r14-r31 onto the stack pointer
 
   mr r14, r0
-  addi r14, r14, 0x4CEA  
-  addi r14, r14, 0x4000  # add 8cea to get us to the right spot in memory
+  addi r14, r14, 0x4000
+  addi r14, r14, 0x4000  # add 8000 to get us to the right spot in memory
+  mr r15, r14            # save register to be further manipulated
 
   lis r23, 0x53          # S
   ori r23, r23, 0x50     # P
@@ -25,8 +26,56 @@
   ori r30, r30, 0x2e     # .
   lis r31, 0x32          # 2
 
-  stmw r23, 0 (r14)
-    
+  stmw r23, 0xcea (r14)
+
+  addi r15, r15, 0x6000  
+  addi r15, r15, 0x6000  
+  addi r15, r15, 0x4000  # add 18000 to r0 to get us to the right spot in memory
+  addi r15, r15, 0x442
+  mr r16, r15            # save register to be further manipulated
+  
+  lis r26, 0x43          # C
+  ori r26, r26, 0x53     # S
+  lis r27, 0x4c          # L
+  ori r27, r27, 0x20     # Space
+  lis r28, 0x53          # S
+  ori r28, r28, 0x54     # T
+  lis r29, 0x41          # A
+  ori r29, r29, 0x44     # D
+  lis r30, 0x49          # I
+  ori r30, r30, 0x41     # A
+  lis r31, 0             # Terminator
+
+  stmw r26, 0x442 (r15)
+  
+  addi r16, r16, 0x6000  
+  addi r16, r16, 0x6000  
+  addi r16, r16, 0x4000  # add 28000 to r0 to get us to the right spot in memory
+  
+  lis r21, 0x4f          # O
+  ori r21, r21, 0x52     # R
+  lis r22, 0x49          # I
+  ori r22, r22, 0x47     # G
+  lis r23, 0x49          # I
+  ori r23, r23, 0x4e     # N
+  lis r24, 0x41          # A
+  ori r24, r24, 0x4C     # L
+  lis r25, 0x20          # Space
+  ori r25, r25, 0x46     # F
+  lis r26, 0x49          # I
+  ori r26, r26, 0x45     # E
+  lis r27, 0x4C          # L
+  ori r27, r27, 0x44     # D
+  lis r28, 0x20          # Space
+  ori r28, r28, 0x53     # S
+  lis r29, 0x50          # P
+  ori r29, r29, 0x45     # E
+  lis r30, 0x45          # E
+  ori r30, r30, 0x44     # D
+  lis r31, 0x53          # S
+
+  stmw r21, 0xecb (r16)
+
   lmw r14, 0x8 (sp)      # pop r14-r31 off the stack pointer
   addi sp, sp, 0x0050    # release the space
   lwz r0, 20 (r1)        # original instruction at 803014AC
