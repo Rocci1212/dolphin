73661905b2910e246c42f0f3925186808fd16a6b
Rocci <mdcoraccio@gmail.com>
2023-08-31 08:03:07 -0400

    updates to custom logging

--- Source/GeckoCodes/CustomLogging.asm
+++ Source/GeckoCodes/CustomLogging.asm
@@ -4,7 +4,7 @@
   # right now this only supports 4 loops, but can do up to 8
   # the first 4 times it runs, r28 is 0-3
   # the next 4 times, r19 is 0-3 while r28 is various mem addresses
-  cmpwi cr3, r28, 3
+  cmplwi cr3, r28, 3
   bgt cr3, FINALLY # escape if we're looking at a mem address
 
   # figure out which loop i'm running
@@ -27,19 +27,22 @@ SET_THE_TABLE:
   mr r21, r8 # backup r8
   mr r22, r9 # backup r9
   mr r23, r10 #backup r10
-
-  lis r24, 0x8000 
-  ori r24, r24, 0x9b34 # set r24 to nlPrintf
-  mtctr r24 # set count register to nlPrintf
-
-  beq LOOP_0
-  b cr1, LOOP_1
-
-LOOP_0: # frame # and player inputs
-  # set data to custom text
+  
+  lis r16, 0x8000 
+  ori r16, r16, 0x9b34 # set r16 to nlPrintf
+  mtctr r16 # set count register to nlPrintf
+  
   lis r3, 0x8060 # set r3 to custom text
   lis r4, 0x8057
   lwz r4, 0xffffd2d0 (r4) # set r4 to the frame #
+
+  beq LOOP_0
+  beq cr1, LOOP_1
+  beq cr2, LOOP_2
+  beq cr3, LOOP_3
+
+LOOP_0: # frame # and player inputs
+  # set data to custom text
 
   lis r10, 0x8058
   ori r10, r10, 0x5e00
@@ -55,9 +58,10 @@ LOOP_0: # frame # and player inputs
 
 LOOP_1: #Ball XYZ values
   # set data to custom text
-  lis r3, 0x8060 # set r3 to custom text
-  lis r4, 0x8057
-  lwz r4, 0xffffd2d0 (r4) # set r4 to the frame #
+  ori r3, r3, 0x20
+
+  li r4, 0
+  ori r4, r4, 0xba11
 
   lis r10, 0x806d
   ori r10, r10, 0xf7a0    # 806df7a0 is a pointer to the ball object
@@ -70,6 +74,21 @@ LOOP_1: #Ball XYZ values
   lwz r10, 0x270 (r10)    # loose ball Z velocity
 
   bctrl # branch and link to nlPrint
+  b CLEAN_UP
+
+LOOP_2: # Random Seeds
+  # set data to custom text
+
+
+  #bctrl # branch and link to nlPrint
+  b CLEAN_UP
+
+
+LOOP_3: # Random Seeds
+  # set data to custom text
+
+
+  #bctrl # branch and link to nlPrint
   b CLEAN_UP
 
 CLEAN_UP:
